d3b4d1b9abe61885875f5a1c78b32875967697ef
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2026-04-14 05:01:29 +0200

    add the "getAllBookings" endpoint, to get all bookings in the system [Admin Only]

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -50,6 +50,62 @@ const getUsersBookings = async (req, res, next) => {
   }
 };
 
+//              ==> GET <==
+// ---- Get All Bookings [Admin ONLY] ----
+const getAllBookings = async (req, res, next) => {
+  try {
+    // Query Parameters
+    const { status, userId, eventId } = req.query;
+
+    // Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    // Filter
+    const filter = {};
+    if (status) filter.status = status;
+    if (userId) {
+      if (!mongoose.Types.ObjectId.isValid(userId))
+        throw AppError.badRequest("Invalid User ID.");
+      filter.userId = userId;
+    }
+    if (eventId) {
+      if (!mongoose.Types.ObjectId.isValid(eventId))
+        throw AppError.badRequest("Invalid Event ID.");
+      filter.eventId = eventId;
+    }
+
+    const bookings = await Booking.find(filter)
+      .populate("userId", "name email")
+      .populate("eventId", "title date location")
+      .skip(skip)
+      .limit(limit);
+
+    const totalNumberOfBookings = await Booking.countDocuments(filter);
+    const totalPages = Math.ceil(totalNumberOfBookings / limit);
+
+    res.status(200).json({
+      success: true,
+      message: "All bookings retrieved successfully",
+      data: {
+        bookings,
+        pagination: {
+          currentPage: page,
+          totalPages,
+          totalBookings: totalNumberOfBookings,
+          limit,
+        },
+      },
+    });
+  } catch (error) {
+    if (error instanceof AppError) return next(error);
+    next(
+      AppError.internalError("An error occurred when retrieving all bookings."),
+    );
+  }
+};
+
 //        ==> GET <==
 // ---- Get Single Bookings ----
 const getSingleBooking = async (req, res, next) => {
@@ -229,4 +285,5 @@ export {
   getUsersBookings,
   getSingleBooking,
   updateBookingStatus,
+  getAllBookings,
 };
